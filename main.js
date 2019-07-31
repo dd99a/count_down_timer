95750850ce565f1c2ef2783a0e93720e58fc5e39
dwei <don.wei@cdev.global>
2019-07-31 16:21:17 -0400

    Allow alarm loop until it has been paused

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 
 var interval;
+var start_minutes = parseInt($('.start-minutes').text());
+
 function countDown() {
     $('button.start').attr('disabled', true);
     $('button.pause').attr('disabled', false);
     interval =setInterval(function(){
          var minutes = parseInt($('.minutes').text());
-         var start_minutes = minutes;
          var seconds = parseInt($(".seconds").text());
          if ( minutes > 0 ) {
              if (seconds === 0) {
@@ -23,12 +24,13 @@ function countDown() {
              seconds--;
              $('.seconds').text( seconds > 10 ? seconds : '0' + seconds);
          } else {
+             document.getElementById("alarm").loop = true;
              document.getElementById("alarm").play();
              Swal.fire({
                  title: 'Good Job',
                  type: 'success',
                  confirmButtonColor: '#3085d6',
-                 confirmButtonText: (start_minutes == 50) ? 'Take a break' : 'Back to work',
+                 confirmButtonText: ((start_minutes === 50) ? 'Take a break' : 'Back to work'),
              }).then((result) => {
                  if (result.value) {
                      document.getElementById("alarm").pause();
@@ -36,7 +38,8 @@ function countDown() {
              })
              clearTimeout(interval);
              $('button.start').attr('disabled', false);
-             $('div.minutes').text((start_minutes == 50) ? '10' : '50');
+             $('div.start-minutes').text((start_minutes === 50) ? '10' : '50');
+             $('div.minutes').text((start_minutes === 50) ? '10' : '50');
              $('div.seconds').text('00');
          }
         }, 1000);
